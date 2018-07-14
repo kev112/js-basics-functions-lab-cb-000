c47e6d98a8df90aece578a44793fc22bce61913a
Tao <werewaswas@live.com>
2018-07-14 21:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(street) {
+function distanceFromHqInBlocks (street) {
   let distance = street - 42
   distance < 0 ? return -distance : return distance
 }
+
+
