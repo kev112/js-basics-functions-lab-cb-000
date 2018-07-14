f6362c69afc5644b610b4b2c779ce7c3b0f0fd69
Tao <werewaswas@live.com>
2018-07-14 21:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ function distanceFromHqInBlocks (street) {
   distance < 0 ? return -distance : return distance
 }
 
-
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
